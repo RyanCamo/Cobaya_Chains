46bd2ce5cd267857e919b1fb1273172a644c11b6
RyanCamo <ryan_camo@hotmail.com>
2022-09-12 09:24:53 +1000

    .

--- likelihoods_SN_interp.py
+++ likelihoods_SN_interp.py
@@ -17,8 +17,8 @@ import time
 ## 3. Models
 
 # Mock Data
-cov_path = Path("data/data_TESTS/cov_wCDM_42.txt")
-HD_path = Path("data/data_TESTS/HD_wCDM_42.txt")
+cov_path = Path("data/data_TESTS/cov_wCDM_14.txt")
+HD_path = Path("data/data_TESTS/HD_wCDM_14.txt")
 arr_size = int(np.genfromtxt(cov_path, comments='#',dtype=None)[0])
 DES5YR_UNBIN = np.genfromtxt(HD_path, names=True, comments='#')
 cov_arr = np.genfromtxt(cov_path, comments='#',dtype=None)[1:]
