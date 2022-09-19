0f3164a1398d27961d5e3189c808ffa62c209a8f
Ryan Camilleri - 1667915 <ryan_camo@hotmail.com>
2022-09-19 12:51:18 +1000

    .

--- BiasCor_Cosmo_Dependencies/Compare_iterations.py
+++ BiasCor_Cosmo_Dependencies/Compare_iterations.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
     # GAL = 5 
 
     mock_data = 2
-    model = FLCDM_ # which fitting model to compare iterations?
+    model = wCDM_ # which fitting model to compare iterations?
     save_path = Path('BiasCor_Cosmo_Dependencies/Comparing_Iterations/Mock:%s_Fit:%s.png' % (mock_data, model.__name__)) # where to save the figure
     compare_cosmo(mock_data, model, save_path)
     #compare_truths()
