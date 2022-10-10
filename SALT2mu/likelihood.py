c2f399c7d6a1e3fe8d61014f6d7f579e75c395ec
Ryan Camilleri - 1667915 <ryan_camo@hotmail.com>
2022-10-11 08:58:42 +1000

    .

--- SALT2mu/likelihood.py
+++ SALT2mu/likelihood.py
@@ -49,7 +49,8 @@ class SALT2mu(Likelihood):
         # This is currently set up using 20 bins. Same as BBC output. (To run a quick MCMC, reduce the bin size...)
         # For 20 bins: [0.010, 0.031, 0.052, 0.076, 0.101, 0.127, 0.156, 0.187, 0.221, 0.258, 0.298, 0.343, 0.392, 0.447, 0.510, 0.581, 0.664, 0.760, 0.877, 1.019, 1.200] 
         # For 1 bin: [0, 1.5] & For 2 bins: [0, 0.1, 1.5]
-        bins = [0.010, 0.031, 0.052, 0.076, 0.101, 0.127, 0.156, 0.187, 0.221, 0.258, 0.298, 0.343, 0.392, 0.447, 0.510, 0.581, 0.664, 0.760, 0.877, 1.019, 1.200] 
+        bins = [0, 0.1, 1.5]
+        #bins = [0.010, 0.031, 0.052, 0.076, 0.101, 0.127, 0.156, 0.187, 0.221, 0.258, 0.298, 0.343, 0.392, 0.447, 0.510, 0.581, 0.664, 0.760, 0.877, 1.019, 1.200] 
         self.dfFITRES['bins'] = np.digitize(self.dfFITRES['zHD'].values, bins) # adds an index to the data defining what bin 
         num_bins = len(bins) - 1 
         chi2_tmp = [] # temp stores the chi2 values for each bin
@@ -71,10 +72,10 @@ class SALT2mu(Likelihood):
             #       This should be ok as the uncertainty on the redshift shouldnt change as I sample alpha/beta/M0DIF
 
             sigma_z2 = df['zHDERR'].values**2 #(5/(np.log(10))) * ( (1+self.dfFITRES['zHD'].values) / (self.dfFITRES['zHD'].values*(1+(self.dfFITRES['zHD'].values/2))) ) * np.sqrt(self.dfFITRES['zHDERR'].values**2 + (self.dfFITRES['VPECERR'].values/self.c)**2)
-            sigma_mu2 = sig_int**2  + sigma_lens2 + sigma_z2 + df['x0ERR'].values**2 + (alpha**2)*df['x1ERR'].values**2 + (beta**2)*df['cERR'].values**2 + 2*alpha*df['COV_x1_x0'].values - 2*beta*df['COV_c_x0'].values - 2*alpha*beta*df['COV_x1_c'].values
+            sigma_mu2 = sig_int**2 - (sig_int*beta*df['zHDERR'].values)**2  + sigma_lens2 + sigma_z2 + df['x0ERR'].values**2 + (alpha**2)*df['x1ERR'].values**2 + (beta**2)*df['cERR'].values**2 + 2*alpha*df['COV_x1_x0'].values - 2*beta*df['COV_c_x0'].values - 2*alpha*beta*df['COV_x1_c'].values
             chi2_tmp.append(np.sum(((mu_data - fid_cosmo)**2)/sigma_mu2 + 2*np.log(np.sqrt(sigma_mu2)))) #- guassian normalization term required if using bias corrections
 
-            #G10 = (0.7*sig_int**2 + (beta**2)*0.3*sig_int**2 - 0.21*beta*sig_int) # my attempt at G10 scatter model.. didnt work.
+            #G10 = sig_int**2 - (sig_int*beta*df['zHDERR'].values)**2 my attempt at simplified G10 scatter model.. fitting now
 
         chi2 = np.sum(chi2_tmp) 
         return -0.5*chi2 
